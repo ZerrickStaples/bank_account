56d5c4b01c02f183fdb1d8eefa8140d52d3307c2
Zerrick Staples <zerrickstaples@Zerricks-MacBook-Pro.local>
2020-05-05 19:27:49 -0400

    Completed application

--- bank_account.py
+++ bank_account.py
@@ -25,4 +25,23 @@ while shopping_status.lower() != 'q':
         else:
             print("You do not have sufficient funds to make this purchase. Please make a deposit. ")
     
+    if choice == 2:
+        if bank_account.check_balance(500) and bank_account.is_positive_balance():
+            bank_account.make_withdrawl(500)
+            print(f'Thank you for your puchase. Your current balance is: ${bank_account.balance}')
+        else:
+            print("You do not have sufficient funds to make this purchase. Please make a deposit. ")
+    
+    if choice == 3:
+        if bank_account.check_balance(10) and bank_account.is_positive_balance():
+            bank_account.make_withdrawl(10)
+            print(f'Thank you for your puchase. Your current balance is: ${bank_account.balance}')
+        else:
+            print("You do not have sufficient funds to make this purchase. Please make a deposit. ")
+    
+    if choice == 4:
+        deposit = input("How much would you like to deposit?: $")
+        bank_account.make_deposit(int(deposit))
+        print(f"Thank you for your deposit. Your balance is ${bank_account.balance} ")
+
     shopping_status = input("Hit enter to continue shopping. Enter Q to quit shopping ")
