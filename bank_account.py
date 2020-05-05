1f2d0bf37b0032748bc23b7a68e629ff0135422f
Zerrick Staples <zerrickstaples@Zerricks-MacBook-Pro.local>
2020-05-05 19:41:41 -0400

    Refactored

--- bank_account.py
+++ bank_account.py
@@ -1,4 +1,4 @@
-from helpers import BankAccount
+from helpers import *
 
 name = input("What is your name? ")
 initial_deposit = int(input("Initial deposit: $"))
@@ -7,6 +7,7 @@ bank_account = BankAccount(name)
 bank_account.make_deposit(initial_deposit)
 
 shopping_status = ''
+
 while shopping_status.lower() != 'q':
     print("""
     Please choose an option: 
@@ -15,33 +16,23 @@ while shopping_status.lower() != 'q':
     3.) Cheap Wine ($10)
     4.) Make a deposit
     """)
-    
+
     choice = int(input())
 
     if choice == 1:
-        if bank_account.check_balance(40) and bank_account.is_positive_balance():
-            bank_account.make_withdrawl(40)
-            print(f'Thank you for your puchase. Your current balance is: ${bank_account.balance}')
-        else:
-            print("You do not have sufficient funds to make this purchase. Please make a deposit. ")
-    
+        grocery_store(bank_account, products["Wine"])
+
     if choice == 2:
-        if bank_account.check_balance(500) and bank_account.is_positive_balance():
-            bank_account.make_withdrawl(500)
-            print(f'Thank you for your puchase. Your current balance is: ${bank_account.balance}')
-        else:
-            print("You do not have sufficient funds to make this purchase. Please make a deposit. ")
-    
+        grocery_store(bank_account, products["Xbox"])
+
     if choice == 3:
-        if bank_account.check_balance(10) and bank_account.is_positive_balance():
-            bank_account.make_withdrawl(10)
-            print(f'Thank you for your puchase. Your current balance is: ${bank_account.balance}')
-        else:
-            print("You do not have sufficient funds to make this purchase. Please make a deposit. ")
-    
+        grocery_store(bank_account, products["Cheap Wine"])
+
     if choice == 4:
         deposit = input("How much would you like to deposit?: $")
         bank_account.make_deposit(int(deposit))
-        print(f"Thank you for your deposit. Your balance is ${bank_account.balance} ")
+        print(
+            f"Thank you for your deposit. Your balance is ${bank_account.balance} ")
 
-    shopping_status = input("Hit enter to continue shopping. Enter Q to quit shopping ")
+    shopping_status = input(
+        "Hit enter to continue shopping. Enter Q to quit shopping ")
--- helpers.py
+++ helpers.py
@@ -15,3 +15,12 @@ class BankAccount(object):
     def is_positive_balance(self):
         return self.balance > 0
             
+products = {"Wine": 40, "Xbox": 500, "Cheap Wine": 10}
+
+def grocery_store(bank_account, amount):
+    if bank_account.check_balance(amount) and bank_account.is_positive_balance():
+        bank_account.make_withdrawl(amount)
+        print(
+            f'Thank you for your puchase. Your current balance is: ${bank_account.balance}')
+    else:
+        print("You do not have sufficient funds to make this purchase. Please make a deposit. ")
