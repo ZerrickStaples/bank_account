f9153e48adf742f33e014feb4a5809c6cba63698
Zerrick Staples <zerrickstaples@Zerricks-MacBook-Pro.local>
2020-05-05 18:18:42 -0400

    Created method to withdraw funds from bank account

--- bank_account.py
+++ bank_account.py
@@ -6,7 +6,11 @@ class BankAccount(object):
     def make_deposit(self, amount):
         self.balance += amount
 
+    def make_withdrawl(self, amount):
+        self.balance -= amount
+
 
 bank_account = BankAccount("Zerrick")
 bank_account.make_deposit(1200)
+bank_account.make_withdrawl(40)
 print(bank_account.__dict__)
