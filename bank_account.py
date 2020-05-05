a94f0152b3458179418995c34f65bb99b681b599
Zerrick Staples <zerrickstaples@Zerricks-MacBook-Pro.local>
2020-05-05 18:16:42 -0400

    Created method to deposit funds into bank account

--- bank_account.py
+++ bank_account.py
@@ -3,5 +3,10 @@ class BankAccount(object):
         self.name = object
         self.balance = 0
 
+    def make_deposit(self, amount):
+        self.balance += amount
+
+
 bank_account = BankAccount("Zerrick")
+bank_account.make_deposit(1200)
 print(bank_account.__dict__)
